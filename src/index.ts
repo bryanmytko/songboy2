c491a01123b92c99d771dc70e1004eef3dbe3a91
Bryan Mytko <bryanmytko@gmail.com>
2022-09-27 17:45:18 -0400

    Add i18n

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+import { Client, GatewayIntentBits } from 'discord.js';
+import { i18n } from './i18n.config';
+
+require('dotenv').config();
+
+const { DISCORD_BOT_TOKEN } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', ():void => {
+  console.log(i18n.__('status.connected'));
+});
+
+client.login(DISCORD_BOT_TOKEN);
+client.on('disconnected', ():void => console.log(i18n.__('status.disconnected')));
