06d6a1643e29155d3c3b466ac6c171c765a17ad2
Bryan Mytko <bryanmytko@gmail.com>
2022-09-28 05:03:07 -0400

    Add tslog

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import { Logger } from 'tslog';
+
 import { i18n } from './i18n.config';
 
 require('dotenv').config();
 
+const log: Logger = new Logger();
 const { DISCORD_BOT_TOKEN } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', ():void => {
-  console.log(i18n.__('status.connected'));
+  log.info(i18n.__('status.connected'));
 });
 
 client.login(DISCORD_BOT_TOKEN);
-client.on('disconnected', ():void => console.log(i18n.__('status.disconnected')));
+
+client.on('disconnected', ():void  => {
+  log.info(i18n.__('status.disconnected'));
+});
