6120b4477d31990dbeb20a480b03f4b387f07e54
Bryan Mytko <bryanmytko@gmail.com>
2022-11-21 00:00:19 -0800

    Add interaction handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import path from "path";
+import fs from "fs";
 import { Logger } from "tslog";
+import { Command } from "./types";
+
 
 import { i18n } from "./i18n.config";
 
@@ -9,6 +13,21 @@ const log: Logger = new Logger();
 const { DISCORD_BOT_TOKEN } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection<string, Command>()
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter((file: any) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
 
 client.once("ready", (): void => {
   log.info(i18n.__("status.connected"));
@@ -18,4 +37,24 @@ client.login(DISCORD_BOT_TOKEN);
 
 client.on("disconnected", (): void => {
   log.info(i18n.__("status.disconnected"));
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  console.log(interaction);
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
