658851ad83e32520c41f0e141d32091ed383ebb7
Bryan Mytko <bryanmytko@gmail.com>
2022-11-20 22:51:44 -0800

    Fix dependencies

--- src/i18n.config.ts
+++ src/i18n.config.ts
@@ -1,9 +1,9 @@
-import { I18n } from 'i18n';
-import path from 'path';
+import { I18n } from "i18n";
+import path from "path";
 
 export const i18n = new I18n({
-  defaultLocale: 'en',
-  directory: path.join(__dirname, 'locales'),
-  locales: ['en'],
-  objectNotation: true
+  defaultLocale: "en",
+  directory: path.join(__dirname, "locales"),
+  locales: ["en"],
+  objectNotation: true,
 });
