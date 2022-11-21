2b1ba26c54801afd48eedb38ec920e9c050c15bc
Bryan Mytko <bryanmytko@gmail.com>
2022-11-20 22:51:44 -0800

    Fix dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import { Logger } from 'tslog';
+import { Client, GatewayIntentBits } from "discord.js";
+import { Logger } from "tslog";
 
-import { i18n } from './i18n.config';
+import { i18n } from "./i18n.config";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const log: Logger = new Logger();
 const { DISCORD_BOT_TOKEN } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', ():void => {
-  log.info(i18n.__('status.connected'));
+client.once("ready", (): void => {
+  log.info(i18n.__("status.connected"));
 });
 
 client.login(DISCORD_BOT_TOKEN);
 
-client.on('disconnected', ():void  => {
-  log.info(i18n.__('status.disconnected'));
+client.on("disconnected", (): void => {
+  log.info(i18n.__("status.disconnected"));
 });
