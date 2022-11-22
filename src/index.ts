5038207416cec826f792846a5005ff364dfc5b58
Bryan Mytko <bryanmytko@gmail.com>
2022-11-21 23:45:20 -0800

    Update i18n calls

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,30 @@ import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import path from "path";
 import fs from "fs";
 import { Logger } from "tslog";
+
 import { Command } from "./types";
-
-
 import { i18n } from "./i18n.config";
 
 require("dotenv").config();
-
 const log: Logger = new Logger();
 const { DISCORD_BOT_TOKEN } = process.env;
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection<string, Command>()
+
+client.commands = new Collection<string, Command>();
 
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter((file: any) => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file: any) => file.endsWith('.ts'));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
+
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command);
   } else {
-    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    log.warn(i18n.__mf("command.missingProperty", filePath));
   }
 }
 
@@ -42,19 +42,18 @@ client.on("disconnected", (): void => {
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
-  console.log(interaction);
-
   const command = interaction.client.commands.get(interaction.commandName);
 
   if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
+    log.error(i18n.__mf("command.missing", interaction.commandName));
     return;
   }
 
   try {
     await command.execute(interaction);
   } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    log.error(error);
+    await interaction
+      .reply({ content: i18n.__("command.error"), ephemeral: true });
   }
 });
