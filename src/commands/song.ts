ddb9e3a30b9b839b2f26ffdeddb77a33fb9fec51
Bryan Mytko <bryanmytko@gmail.com>
2022-11-25 01:47:10 -0800

    Add source enum

--- src/commands/song.ts
+++ src/commands/song.ts
@@ -1,34 +1,61 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Logger } from "tslog";
 
 import Player from "../lib/player";
 import { i18n } from "../i18n.config";
 
+const log: Logger = new Logger();
+
+enum Source {
+  Random,
+  Reconnect,
+  Request,
+}
+
 const player = new Player();
 
-const execute = async (interaction: CommandInteraction) => {
-  console.log(interaction)
-  /* TODO guildId should pass to Player instead of relying on interaction */
-  const { guildId } = interaction;
+const execute = async (
+  interaction: ChatInputCommandInteraction,
+  message: string
+) => {
+  const { guild } = interaction;
+  const memberId = interaction.member?.user.id || "";
 
-  const memberId = interaction.member?.user.id || '';
-  const voiceChannelId = interaction
-    .guild?.members.cache.get(memberId)?.voice.channelId;
+  console.log(message);
+
+  if (!guild) {
+    log.error(i18n.__("commands.song.errors.missingGuild"));
+    return interaction.reply(i18n.__("commands.song.noGuild"));
+  }
+
+  const voiceChannelId =
+    guild.members.cache.get(memberId)?.voice.channelId;
+
+  if (!voiceChannelId) {
+    return interaction.reply(i18n.__("commands.song.noVoiceChannel"));
+  }
 
   await interaction.reply("Queueing or playing a song!");
 
   /* TODO placeholder */
   player.addSong({
     input: "Foo Fighters - The Best",
-    message: "This one is great! ",
-    source: "Input",
+    message: "",
+    source: Source.Request,
     voiceChannelId,
-    interaction
+    guildId: guild.id,
+    guild,
   });
 };
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName(i18n.__("commands.song.name"))
-    .setDescription(i18n.__("commands.song.desc")),
+    .setDescription(i18n.__("commands.song.desc"))
+    .addStringOption(option =>
+      option
+        .setName('search')
+        .setDescription('Search string.')
+        .setRequired(true)),
   execute,
 };
