acba4af993ea7b37ca9ae06cd9f320a76a4ad987
Bryan Mytko <bryanmytko@gmail.com>
2022-11-25 23:53:39 -0800

    Get query string

--- src/commands/song.ts
+++ src/commands/song.ts
@@ -1,27 +1,19 @@
-import { ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { Logger } from "tslog";
 
 import Player from "../lib/player";
 import { i18n } from "../i18n.config";
+import { Source } from "../types";
 
 const log: Logger = new Logger();
-
-enum Source {
-  Random,
-  Reconnect,
-  Request,
-}
-
-const player = new Player();
+const player: Player = new Player();
 
 const execute = async (
   interaction: ChatInputCommandInteraction,
-  message: string
+  query: string
 ) => {
   const { guild } = interaction;
   const memberId = interaction.member?.user.id || "";
-
-  console.log(message);
 
   if (!guild) {
     log.error(i18n.__("commands.song.errors.missingGuild"));
@@ -35,12 +27,15 @@ const execute = async (
     return interaction.reply(i18n.__("commands.song.noVoiceChannel"));
   }
 
-  await interaction.reply("Queueing or playing a song!");
+  /* This should instead display the reult of the query.
+     We can accomplish this by either passing interaction and having the
+     reply trigger within `addSong` or use a return value. */
+  await interaction.reply(`Adding ${query} to the playlist`);
 
   /* TODO placeholder */
   player.addSong({
-    input: "Foo Fighters - The Best",
-    message: "",
+    query,
+    message: "", // This will be DJ hook. Might just move this inside Player
     source: Source.Request,
     voiceChannelId,
     guildId: guild.id,
@@ -54,8 +49,8 @@ module.exports = {
     .setDescription(i18n.__("commands.song.desc"))
     .addStringOption(option =>
       option
-        .setName('search')
-        .setDescription('Search string.')
+        .setName(i18n.__("commands.song.search.name"))
+        .setDescription(i18n.__("commands.song.search.desc"))
         .setRequired(true)),
   execute,
 };
