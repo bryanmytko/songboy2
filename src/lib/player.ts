9cdbf27c7180aeae481c3a491d96a056ecc23de7
Bryan Mytko <bryanmytko@gmail.com>
2022-11-26 17:08:44 -0800

    Add songService

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -1,9 +1,11 @@
 import { joinVoiceChannel, VoiceConnectionStatus } from "@discordjs/voice";
 import { Logger } from "tslog";
+import SongService from "../lib/songService";
 
 import { SongParams } from "../types/player";
 
 const log: Logger = new Logger();
+const songService: SongService = new SongService();
 
 class Player {
   constructor() {
@@ -11,7 +13,8 @@ class Player {
   }
 
   async addSong(params: SongParams) {
-    const { guild, guildId, voiceChannelId } = params;
+    const { guild, guildId, voiceChannelId, query } = params;
+    const song = this.searchSong(query);
 
     const connection = joinVoiceChannel({
       channelId: voiceChannelId,
@@ -22,9 +25,17 @@ class Player {
     connection.on(VoiceConnectionStatus.Ready, () => {
       /* Placeholder */
       log.info(
-        "The connection has entered the Ready state. ready to play audio!"
+        `The connection has entered the Ready state. ready to play ${song}`
       );
     });
+  };
+
+  async searchSong(query: string) {
+    try {
+      return songService.searchVideos(query);
+    } catch (err) {
+      log.error('Song search failed to return a response');
+    }
   }
 }
 
---
+++ src/lib/songService.ts
@@ -0,0 +1,7 @@
+class SongService {
+  async searchVideos(query: string) {
+    return "Hello!" + query;
+  }
+}
+
+export default SongService;
