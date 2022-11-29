ad91dcc6c020cb769ca3c7f2e390eb77a414e52f
Bryan Mytko <bryanmytko@gmail.com>
2022-11-28 20:31:39 -0800

    Remove unused

--- src/types/player.ts
+++ src/types/player.ts
@@ -1,5 +1,3 @@
-import { Guild } from "discord.js";
-
 export enum Source {
   Random,
   Reconnect,
@@ -7,10 +5,7 @@ export enum Source {
 };
 
 export interface SongParams {
-  // guild: Guild;
-  // guildId: string;
   message: string;
   query: string;
   source: Source;
-  // voiceChannelId: string;
 };
