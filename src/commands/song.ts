86e2763f51c5cfe57c7f9679ed72dad902e2a98c
Bryan Mytko <bryanmytko@gmail.com>
2022-11-28 20:30:56 -0800

    Pass in interaction for replies

--- src/commands/song.ts
+++ src/commands/song.ts
@@ -1,14 +1,18 @@
-import { AttachmentBuilder, ChatInputCommandInteraction, GuildMember, SlashCommandBuilder, Snowflake, VoiceChannel } from "discord.js";
+import {
+  AttachmentBuilder,
+  ChatInputCommandInteraction,
+  GuildMember,
+  SlashCommandBuilder,
+  Snowflake
+} from "discord.js";
 import { Logger } from "tslog";
 
 import Player from "../lib/player";
 import { i18n } from "../i18n.config";
 import { Source } from "../types/player";
 import { joinVoiceChannel } from "@discordjs/voice";
-import { channel } from "diagnostics_channel";
 
 const log: Logger = new Logger();
-//const player: Player = new Player();
 const players = new Map<Snowflake, Player>();
 
 const execute = async (
@@ -16,8 +20,6 @@ const execute = async (
   query: string
 ) => {
   const { guildId } = interaction;
-  const memberId = interaction.member?.user.id || "";
-
   if (!guildId) return log.error("No guildID found!");
 
   let player = players.get(guildId);
@@ -31,7 +33,7 @@ const execute = async (
         channelId: channel.id,
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
-      }));
+      }), interaction);
 
       player.voiceConnection.on("error", console.warn);
       players.set(guildId, player);
@@ -42,16 +44,12 @@ const execute = async (
     .followUp(i18n.__("commands.song.noVoiceChannel"));
 
   const song = await player.addSong({
-    message: "", // This will be DJ hook. Might just move this inside Player
+    message: "", // This is for DJ lead
     query,
     source: Source.Request,
   });
 
   player.addToQueue(song);
-
-  //await interaction.reply(`Adding ${song.title} to the playlist`);
-  // const file = new AttachmentBuilder(song.thumbnail);
-  // await interaction.reply({ content: "hi", files: [file] });
 };
 
 module.exports = {
