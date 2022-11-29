59279a5674ec9c186a6c5162fb0e605bca9f32be
Bryan Mytko <bryanmytko@gmail.com>
2022-11-28 20:31:28 -0800

    Add getReadableStream

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -1,64 +1,59 @@
-import { AudioPlayer, createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnection, VoiceConnectionStatus } from "@discordjs/voice";
-import { Snowflake } from "discord.js";
-import internal from "stream";
+import {
+  AudioPlayer,
+  createAudioPlayer,
+  createAudioResource,
+  VoiceConnection,
+} from "@discordjs/voice";
+import { AttachmentBuilder, ChatInputCommandInteraction } from "discord.js";
 import { Logger } from "tslog";
 
+import { i18n } from "../i18n.config";
 import SongService from "../lib/songService";
 import { SongParams } from "../types/player";
 
 const log: Logger = new Logger();
-const songService: SongService = new SongService();
-const audioPlayer = createAudioPlayer();
 
 interface Song {
-  stream: internal.Readable
-  title: string
-  thumbnail: string
+  videoId: string;
+  title: string;
+  thumbnail: string;
 }
 
 class Player {
   public readonly voiceConnection: VoiceConnection;
+  public readonly interaction: ChatInputCommandInteraction;
   public readonly audioPlayer: AudioPlayer;
+  public readonly songService: SongService;
   public queue: Song[];
 
-  constructor(voiceConnection: VoiceConnection) {
+  constructor(
+    voiceConnection: VoiceConnection,
+    interaction: ChatInputCommandInteraction
+  ) {
     this.voiceConnection = voiceConnection;
+    this.interaction = interaction;
     this.audioPlayer = createAudioPlayer();
+    this.songService = new SongService();
     this.queue = [];
-
-    // this.audioPlayer.on("stateChange", (oldState: { status: any; resource: any; }, newState: { status: any; resource: any; }) => {
-    //   if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
-    //     // If the Idle state is entered from a non-Idle state, it means that an audio resource has finished playing.
-    //     // The queue is then processed to start playing the next track, if one is available.
-    //     (oldState.resource as AudioResource<Track>).metadata.onFinish();
-    //     void this.processQueue();
-    //   } else if (newState.status === AudioPlayerStatus.Playing) {
-    //     // If the Playing state has been entered, then a new track has started playback.
-    //     (newState.resource as AudioResource<Track>).metadata.onStart();
-    //   }
-    // });
-
-    // this.audioPlayer.on('error', (error: { resource: any; }) => (error.resource as AudioResource<Track>).metadata.onError(error));
 
     voiceConnection.subscribe(this.audioPlayer);
   }
 
   async addSong(params: SongParams): Promise<Song> {
+    /* This will also eventually handle:
+        - DJ hook
+        - DB logging
+        - Reconnect logic
+    */
+
     const { query } = params;
-    const song = await this.searchSong(query);
 
-    if (!song) throw new Error('Something went wrong. Could not add song.');
-
-    // const resource = createAudioResource(song.song);
-
-    // connection.on(VoiceConnectionStatus.Ready, () => {
-    //   audioPlayer.play(resource);
-    //   connection.subscribe(audioPlayer);
-    // });
-
-
-    return song;
-  };
+    try {
+      return this.songService.searchVideos(query);
+    } catch {
+      throw new Error("Something went wrong. Could not add song.");
+    }
+  }
 
   public addToQueue(song: Song) {
     this.queue.push(song);
@@ -70,26 +65,25 @@ class Player {
     this.audioPlayer.stop(true);
   }
 
-  private processQueue() {
+  private async processQueue() {
     if (this.queue.length === 0) {
-      log.info('Queue is empty!');
+      return this.interaction.reply(i18n.__("commands.song.queueEmpty"));
     }
 
     const nextSong = this.queue.shift()!;
-    const resource = createAudioResource(nextSong.stream);
+    const stream = await this.songService.getReadableStream(nextSong.videoId);
+    const resource = createAudioResource(stream);
 
     try {
+      const file = new AttachmentBuilder(nextSong.thumbnail);
+
+      await this.interaction.reply({
+        content: i18n.__mf("commands.song.playing", nextSong.title),
+        files: [file],
+      });
       this.audioPlayer.play(resource);
     } catch (error) {
       this.processQueue();
-    }
-  }
-
-  private async searchSong(query: string) {
-    try {
-      return songService.searchVideos(query);
-    } catch (err) {
-      log.error('Song search failed to return a response');
     }
   }
 }
