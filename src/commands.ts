a1b5fe7f8f725d4531ee9cf137e65085ad3d6418
Bryan Mytko <bryanmytko@gmail.com>
2022-11-28 18:00:18 -0800

    Formatting

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,8 @@
-import { CommandInteraction, ChatInputApplicationCommandData, Client } from "discord.js";
+import {
+  CommandInteraction,
+  ChatInputApplicationCommandData,
+  Client
+} from "discord.js";
 
 export interface Command extends ChatInputApplicationCommandData {
   run: (client: Client, interaction: CommandInteraction) => void;
