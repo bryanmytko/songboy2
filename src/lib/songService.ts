471ec85c80673b014111913fc7d186e61fca6ba9
Bryan Mytko <bryanmytko@gmail.com>
2022-11-30 02:13:20 -0800

    Parse JSON quotes

--- src/lib/songService.ts
+++ src/lib/songService.ts
@@ -2,44 +2,41 @@ import internal from "stream";
 import { Logger } from "tslog";
 import Youtube from "youtube.ts";
 
-const log: Logger = new Logger();
-
-const { GOOGLE_API_KEY } = process.env;
-
-interface SongService {
-  youtube: Youtube;
-}
-
 interface SongType {
   videoId: string;
   title: string;
   thumbnail: string;
 }
 
+const log: Logger = new Logger();
+
 class SongService {
+  public readonly youtube: Youtube;
   constructor() {
-    try {
-      this.youtube = new Youtube(GOOGLE_API_KEY);
-    } catch (e) {
-      log.error("Invalid or missing YouTube API key!");
-    }
+    const { GOOGLE_API_KEY } = process.env;
+    this.youtube = new Youtube(GOOGLE_API_KEY);
   }
 
   async searchVideos(query: string): Promise<SongType> {
-    const videoSearch = await this.youtube.videos.search({
-      q: query,
-      maxResults: 1,
-    });
+    try {
+      const videoSearch = await this.youtube.videos.search({
+        q: query,
+        maxResults: 1,
+      });
 
-    const result = videoSearch.items[0];
-    const { snippet } = result;
-    const { videoId } = result.id;
+      const result = videoSearch.items[0];
+      const { snippet } = result;
+      const { videoId } = result.id;
 
-    return {
-      videoId,
-      title: snippet.title || "",
-      thumbnail: snippet.thumbnails.high.url || "",
-    };
+      return {
+        videoId,
+        title: snippet.title.replace(/&quot;/g, '"') || "",
+        thumbnail: snippet.thumbnails.high.url || "",
+      };
+    } catch (e: any) {
+      log.error(e.message);
+      throw new Error();
+    }
   }
 
   async getReadableStream(videoId: string): Promise<internal.Readable> {
