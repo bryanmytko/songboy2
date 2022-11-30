e4ec9d1268ca1f536a5ca28626f9211aa4fac3a5
Bryan Mytko <bryanmytko@gmail.com>
2022-11-30 02:12:14 -0800

    Move player to command context

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,97 @@
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  GuildMember,
+  Snowflake,
+} from "discord.js";
+import { joinVoiceChannel, VoiceConnectionStatus } from "@discordjs/voice";
 import path from "path";
 import fs from "fs";
 import { Logger } from "tslog";
 
 import { Command } from "./types";
 import { i18n } from "./i18n.config";
-import { VoiceConnectionStatus } from "@discordjs/voice";
+import Player from "./lib/player";
+import { Source } from "./types/player";
 
 require("dotenv").config();
 const { DISCORD_BOT_TOKEN } = process.env;
 
 const log: Logger = new Logger();
+const players = new Map<Snowflake, Player>();
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates
-  ]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
-
 client.commands = new Collection<string, Command>();
 
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file: any) => file.endsWith('.ts'));
+  .filter((file: any) => file.endsWith(".ts"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
 
-  if ('data' in command && 'execute' in command) {
+  if ("data" in command && "execute" in command) {
     client.commands.set(command.data.name, command);
   } else {
     log.warn(i18n.__mf("command.missingProperty", filePath));
   }
 }
 
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, (c) => {
   log.info(i18n.__mf("status.connected", c.user.username));
 });
 
 client.login(DISCORD_BOT_TOKEN);
 
-client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async (interaction) => {
+  const { channel: textChannel, guildId } = interaction;
+
+  if (!guildId || !textChannel || !interaction.isChatInputCommand()) return;
 
   const command = interaction.client.commands.get(interaction.commandName);
+  const query = interaction.options.getString("search");
 
-  if (!command) {
-    log.error(i18n.__mf("command.missing", interaction.commandName));
+  let player = players.get(guildId);
+
+  if (!player) {
+    if (
+      interaction.member instanceof GuildMember &&
+      interaction.member.voice.channel
+    ) {
+      const { channel } = interaction.member.voice;
+
+      player = new Player(
+        joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+        }),
+        textChannel
+      );
+
+      player.voiceConnection.on("error", console.warn);
+      players.set(guildId, player);
+    }
+  }
+
+  if (!player) {
+    interaction.reply(i18n.__("commands.song.noVoiceChannel"));
     return;
   }
 
-  const query = interaction.options.getString("search");
-
   try {
-    await command.execute(interaction, query);
+    await command.execute(interaction, query, player);
   } catch (error) {
-    log.error(error);
-    await interaction
-      .reply({ content: i18n.__("command.error"), ephemeral: true });
+    await interaction.reply({
+      content: i18n.__("command.error"),
+      ephemeral: true,
+    });
   }
 });
 
