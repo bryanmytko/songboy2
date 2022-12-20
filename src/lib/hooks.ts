27ba164a85d0c0045e0df16fc09ca8e977156c07
Bryan Mytko <bryanmytko@gmail.com>
2022-12-19 18:12:22 -0800

    Add randomSong hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,18 +1,23 @@
 import textToSpeech from "@google-cloud/text-to-speech";
 import { google } from "@google-cloud/text-to-speech/build/protos/protos";
 import { Readable } from "stream";
-import { Song } from "../types/player";
+import { Song, Source } from "../types/player";
 import { randomVoice } from "./tts/voices";
-import { randomLead } from "./tts/leads";
+import { randomLead, randomSongLead } from "./tts/leads";
 
 const client = new textToSpeech.TextToSpeechClient();
 
-const getHook = async (song: Song) =>
-  synthesizedSpeechStream({
-    input: { text: randomLead(song.title, song.requester) },
+const getHook = async (song: Song) => {
+  const text = (song.source === Source.Random) ?
+    randomSongLead(song.title, song.requester) :
+    randomLead(song.title, song.requester);
+
+  return synthesizedSpeechStream({
+    input: { text },
     voice: randomVoice(),
     audioConfig: { audioEncoding: "MP3", speakingRate: 1.0 },
   });
+};
 
 const synthesizedSpeechStream = async (
   request: google.cloud.texttospeech.v1.ISynthesizeSpeechRequest
