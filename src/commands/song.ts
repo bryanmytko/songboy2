495b46274bc23b005f1d7caf5b26b991096470ff
Bryan Mytko <bryanmytko@gmail.com>
2023-01-16 22:15:41 -0800

    Add check for voice connection status to prevent failing on private channels

--- src/commands/song.ts
+++ src/commands/song.ts
@@ -1,9 +1,12 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Logger } from "tslog";
 
 import Player from "../lib/player";
 import { saveSongHistory } from "../lib/history";
 import { i18n } from "../i18n.config";
 import { songService } from "../lib/songService";
+
+const log: Logger = new Logger();
 
 const execute = async (
   interaction: ChatInputCommandInteraction,
@@ -14,9 +17,15 @@ const execute = async (
   const result = await songService.searchVideos(query);
   const song = { ...result, requester: username };
 
-  interaction.reply(i18n.__mf("commands.song.added", song.title));
-  player.play(song);
-  void saveSongHistory(song);
+  if (player.voiceConnection.state.status === "signalling") {
+    log.info(`${username} tried to request a song from a private chanel.`);
+    interaction.reply(i18n.__("commands.song.channelPermission"));
+    player.voiceConnection.destroy();
+  } else {
+    interaction.reply(i18n.__mf("commands.song.added", song.title));
+    player.play(song);
+    void saveSongHistory(song);
+  }
 };
 
 module.exports = {
