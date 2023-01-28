1c51f65f81c72f2ce8047cead5692b4e9d776112
Bryan Mytko <bryanmytko@gmail.com>
2023-01-28 03:54:44 -0800

    Add random function

--- src/models/song.ts
+++ src/models/song.ts
@@ -1,8 +1,8 @@
 import { model, Schema } from "mongoose";
 
 interface ISongCount {
-  title: string,
-  count: number
+  title: string;
+  count: number;
 }
 
 const schema = new Schema({
@@ -30,45 +30,55 @@ const schema = new Schema({
 
 const Song = model("Song", schema);
 
-const topTen = async (): Promise<ISongCount[]> => await Song.aggregate([
-  { $group: { _id: "$title", count: { $sum: 1 } } },
-  { $sort: { count: -1 } },
+const random = async () => {
+  const count = await Song.countDocuments();
+  const random = Math.floor(Math.random() * count);
 
-  { $group: { _id: 1, title: { $push: { title: "$_id", count: "$count" } } } },
-  {
-    $project: {
-      first: { $arrayElemAt: ["$title", 0] },
-      second: { $arrayElemAt: ["$title", 1] },
-      third: { $arrayElemAt: ["$title", 2] },
-      fourth: { $arrayElemAt: ["$title", 3] },
-      fifth: { $arrayElemAt: ["$title", 4] },
-      sixth: { $arrayElemAt: ["$title", 5] },
-      seventh: { $arrayElemAt: ["$title", 6] },
-      eighth: { $arrayElemAt: ["$title", 7] },
-      ninth: { $arrayElemAt: ["$title", 8] },
-      tenth: { $arrayElemAt: ["$title", 9] },
-    }
-  },
+  return Song.findOne().skip(random);
+};
 
-  {
-    $project: {
-      status: [
-        "$first",
-        "$second",
-        "$third",
-        "$fourth",
-        "$fifth",
-        "$sixth",
-        "$seventh",
-        "$eighth",
-        "$ninth",
-        "$tenth"
-      ]
-    }
-  },
+const topTen = async (): Promise<ISongCount[]> =>
+  await Song.aggregate([
+    { $group: { _id: "$title", count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
 
-  { $unwind: "$status" },
-  { $project: { _id: 0, title: "$status.title", count: "$status.count" } }
-]);
+    {
+      $group: { _id: 1, title: { $push: { title: "$_id", count: "$count" } } },
+    },
+    {
+      $project: {
+        first: { $arrayElemAt: ["$title", 0] },
+        second: { $arrayElemAt: ["$title", 1] },
+        third: { $arrayElemAt: ["$title", 2] },
+        fourth: { $arrayElemAt: ["$title", 3] },
+        fifth: { $arrayElemAt: ["$title", 4] },
+        sixth: { $arrayElemAt: ["$title", 5] },
+        seventh: { $arrayElemAt: ["$title", 6] },
+        eighth: { $arrayElemAt: ["$title", 7] },
+        ninth: { $arrayElemAt: ["$title", 8] },
+        tenth: { $arrayElemAt: ["$title", 9] },
+      },
+    },
 
-export { Song, topTen, ISongCount };
+    {
+      $project: {
+        status: [
+          "$first",
+          "$second",
+          "$third",
+          "$fourth",
+          "$fifth",
+          "$sixth",
+          "$seventh",
+          "$eighth",
+          "$ninth",
+          "$tenth",
+        ],
+      },
+    },
+
+    { $unwind: "$status" },
+    { $project: { _id: 0, title: "$status.title", count: "$status.count" } },
+  ]);
+
+export { Song, random, topTen, ISongCount };
